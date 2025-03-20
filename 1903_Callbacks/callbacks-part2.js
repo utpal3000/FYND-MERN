386d4dd6261b4f26a3c0638560741c0dade11cdf
Utpalkant Sahu <89702252+utpal3000@users.noreply.github.com>
2025-03-20 22:13:06 +0530

    Create callbacks-part2.js

---
+++ 1903_Callbacks/callbacks-part2.js
@@ -0,0 +1,15 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
+
+function hello(fun){
+    fun()
+}
+let sum= (a,b)=>{console.log(a+b)}
+
+hello(()=>{sum(10,20)})
+
+// Use anonymous/ no-name function to pass a function with arguments in a function and use it for callbacks.
+
+// Without execution log me b kuch ni hoga!
